e4a2756ae6758abe727fa9fdd05fb14632969d53
omar kolsi <omar.kolsi1@esprit.tn>
2025-03-20 11:14:45 +0100

    crud nutrition

---
+++ bambinou/Service/INutritionService.java
@@ -0,0 +1,13 @@
+package tn.esprit.bambinou.Service;
+
+import tn.esprit.bambinou.Entity.Nutrition;
+import java.util.List;
+
+public interface INutritionService {
+    public List<Nutrition> retrieveAllNutritions();
+    public Nutrition retrieveNutrition(Long id);
+    public Nutrition addNutrition(Nutrition nutrition);
+    public void removeNutrition(Long id);
+    public Nutrition modifyNutrition(Nutrition nutrition);
+    List<Nutrition> getNutritionsByUser(Long idUser);
+}
