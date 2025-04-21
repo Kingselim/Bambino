8eadf34056bba0f2075c1ffe8d4fb7e180bfa406
Kingselim <gaaloul.selim@esprit.tn>
2025-04-21 17:49:59 +0100

    userSpringbootSpring

--- src/main/java/tn/esprit/bambinou/Repository/ForumRepository.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.bambinou.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.esprit.bambinou.Entity.Forum;
-
-public interface ForumRepository extends JpaRepository<Forum, Long> {
-}
